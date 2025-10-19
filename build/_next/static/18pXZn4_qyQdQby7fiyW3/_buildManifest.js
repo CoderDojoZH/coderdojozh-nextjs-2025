cdd87398c84c3ad735b9a6fb9438fba456f2f71e
bibix <bibix@>
2025-10-19 15:32:10 +0200

    Test deployment

---
+++ build/_next/static/18pXZn4_qyQdQby7fiyW3/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/aae3c02f815335e0.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [
+      {
+        "source": "/",
+        "destination": "/de"
+      }
+    ],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ build/_next/static/chunks/aae3c02f815335e0.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/1457f6a85407e3c5.js",
+  "static/chunks/turbopack-42434189100e377a.js"
+])
